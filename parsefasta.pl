9d16f3f0e56d869c6709aa2d3ed45e7fa859742f
Craig Monger <craig@bioinfo-wstation-01.nibrt.ie>
2015-05-27 02:35:20 +0100

    Can now search a blast database for offtarget effects

--- parsefasta.pl
+++ parsefasta.pl
@@ -47,14 +47,65 @@ close $log;
 		system "blastn -db cho_mrna -word_size 16 -evalue 1000 -query temp.fa > temp.bln";
 		
 		#Parse the blast output and retain any useful information	
+		#lazy use of system grep
+		system("grep -A 1 \">\" temp.bln > tmp.txt");
+		open(fi, "<tmp.txt");
+		my @text = <fi>;
+		close fi;
+		my @titles = ();
+		for (my $i=0; $i < scalar(@text); $i++) 
+			{
+			my $line = $text[$i];
+			chomp($line);
+			my $tmptitle;
+			if ($line =~ />/) 
+				{
+				$tmptitle .= $line;	
+				$line = $text[$i+1];
+				chomp($line);
+				$tmptitle .= " ".$line;
+				push(@titles, $tmptitle);
+				}						
+       			}
+		#array @titles now contains each blast hit for the siRNA candidate, can look for genes now		 
+		my @blasthits;
+        	foreach (@titles)
+                #count number of ()
+               		 {
+                	if ($_ =~ m/(\(.+\))/g)
+                        	{
+                        	push (@blasthits, $1) ;
+                        	}
+                	}
+        	my @uniqblasthits= uniq(@blasthits);
+#       	print @uniqblasthits,"\n\n\n";
 
-		system "cat runlog.txt temp.fa > runlogtemp.txt";
-		system "mv runlogtemp.txt runlog.txt";
+        	if (scalar @uniqblasthits == 1)
+                	{
+                	print "\n>$genename\_candidate_siRNA_\#$count has no off-target effects\n\n"
+                	}
+	
+
+		}	
+#		print scalar @titles, "\n";
+		system("rm tmp.txt");
+		#array @titles now contains each blast hit for the siRNA candidate, can look for genes now
+}	
 		
-		}
+
+
+#Subroutines
+
+sub uniq {
+    my %seen;
+    grep !$seen{$_}++, @_;
 }
 
 
 #Should add the ability to take a remote fasta sequence as entry (or GI)
 #Next thing to do is to make it write a new fasta containing the candidates, and to record the name of the input mRNA
 #Add the ability to check thermostability, GC content, binding to loop regions of mRNA
+
+
+#Ensure that only the coding region is searched (Look for an ORF)
+#Score candidates using rational rnai guidelines http://www.protocol-online.org/prot/Protocols/Rules-of-siRNA-design-for-RNA-interference--RNAi--3210.html
